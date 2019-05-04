d2c325ca913f4094745646d9778ce4784b10e624
xMahmouD <50258825+xMahmouD@users.noreply.github.com>
2019-05-04 11:13:39 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = 'F';
+const prefix = '/';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['Foxmc Server ','FoxMC Community'];
+    var setGame = ['Test  ','Test Bot'];
     var i = -1;
     var j = 0;
     setInterval(function (){
